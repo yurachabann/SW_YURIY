1cf96a474f16283d1ed0d90427e04b09e5336a79
Daniem01 <danimartindc01@gmail.com>
2025-04-07 10:34:02 +0200

    copias de seguridad de la bdd y gitignore

---
+++ Practica3/data/01-crea.sql
@@ -0,0 +1,28 @@
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS "Mazos";
+CREATE TABLE "Mazos" (
+	"id"	INTEGER,
+	"nombre"	TEXT NOT NULL,
+	"creador"	TEXT NOT NULL,
+	"cartas"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "Usuarios";
+CREATE TABLE "Usuarios" (
+	"id"	INTEGER NOT NULL,
+	"username"	TEXT NOT NULL UNIQUE,
+	"nombre"	TEXT NOT NULL,
+	"rol"	TEXT NOT NULL DEFAULT 'U' CHECK("rol" IN ('U', 'A')),
+	"password"	TEXT NOT NULL,
+	"email"	TEXT NOT NULL DEFAULT 'test@gmail.com',
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "cartas";
+CREATE TABLE "cartas" (
+	"id"	INTEGER,
+	"nombre"	TEXT NOT NULL,
+	"fuerza"	INTEGER NOT NULL,
+	"tipocarta"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+COMMIT;
