b500dac620b9321418ed5e0ff7f1f8ad7c296071
Yuriy Chaban Markevych <92891311+yurachabann@users.noreply.github.com>
2025-04-07 14:01:46 +0200

    usuarios/admins solo acceden a lo que deberian + comentarios en las paginas

--- Practica3/src/contenido/controllers.js
+++ Practica3/src/contenido/controllers.js
@@ -1,8 +1,8 @@
 export function viewContenidoNormal(req, res) {
-    let contenido = 'paginas/noPermisos';
-    if (req.session != null && req.session.nombre != null) {
-        contenido = 'paginas/normal';
-    }
+    let contenido = 'paginas/normal';
+  //  if (req.session != null && req.session.nombre != null) {
+       // contenido = 'paginas/normal';
+  //  }
     res.render('pagina', {
         contenido,
         session: req.session
@@ -11,7 +11,10 @@ export function viewContenidoNormal(req, res) {
 
 export function viewContenidoAdmin(req, res) {
     let contenido = 'paginas/noPermisos';
-    if (req.session != null && req.session.login && req.session.esAdmin) {
+    if(req.session.login == undefined) {
+        contenido = 'paginas/noPermisosLogin'
+    }
+    else if (req.session != null && req.session.login && req.session.esAdmin) {
         contenido = 'paginas/admin';
     }
     res.render('pagina', {
@@ -22,7 +25,13 @@ export function viewContenidoAdmin(req, res) {
 
 export function viewContenidoUser(req, res) {
     let contenido = 'paginas/user';
-
+    if(req.session.login == undefined) {
+        contenido = 'paginas/noPermisosLogin'
+    }
+    else if (req.session != null && req.session.login && req.session.esAdmin) {
+        contenido = 'paginas/noPermisosUsuario';
+    }
+    console.log(req.session);
     res.render('pagina', {
         contenido,
         session: req.session
