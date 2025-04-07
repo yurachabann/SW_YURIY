4d826f8449ca167505542f768f32771d964c0391
Daniem01 <danimartindc01@gmail.com>
2025-04-07 11:34:54 +0200

    gitignore y nuevo protocolo de mensajes en el login

--- Practica3/src/usuarios/controllers.js
+++ Practica3/src/usuarios/controllers.js
@@ -1,5 +1,5 @@
-import { body } from 'express-validator';
 import { Usuario, RolesEnum } from './Usuario.js';
+import { body, validationResult } from 'express-validator';
 
 export function viewLogin(req, res) {
     let contenido = 'paginas/login';
@@ -51,29 +51,49 @@ export function viewAdd(req, res) {
 }
 
 export function doLogin(req, res) {
-    body('username').escape();
-    body('password').escape();
-    // Capturo las variables username y password
-    const username = req.body.username.trim();
-    const password = req.body.password.trim();
+    //No valen campos vacíos
+    const validations = [
+        body('username', 'El nombre de usuario es obligatorio').notEmpty(),
+        body('password', 'La contraseña es obligatoria').notEmpty(),
+    ];
 
-    try {
-        const usuario = Usuario.login(username, password);
-        req.session.login = true;
-        req.session.nombre = usuario.nombre;
-        req.session.esAdmin = usuario.rol === RolesEnum.ADMIN;
+    //Validaciones
+    Promise.all(validations.map(validation => validation.run(req))).then(() => {
+        const errors = validationResult(req);
+        //Si hay error devolverlo al usuario (errores tiene algo dentro)
+        if (!errors.isEmpty()) {
+            return res.render('pagina', {
+                contenido: 'paginas/login',
+                //Mostrar los errores que haya
+                error: errors.array().map(err => err.msg).join('. '), 
+            });
+        }
 
-        return res.render('pagina', {
-            contenido: 'paginas/home',
-            session: req.session
-        });
+        //Capturar username y password
+        const username = req.body.username.trim();
+        const password = req.body.password.trim();
 
-    } catch (e) {
-        res.render('pagina', {
-            contenido: 'paginas/login',
-            error: 'El usuario o contraseña no son válidos'
-        })
-    }
+        try {
+            //Verificamos si el usuario existe y si la contraseña es la correcta
+            const usuario = Usuario.login(username, password);
+            req.session.login = true;
+            req.session.nombre = usuario.nombre;
+            req.session.esAdmin = usuario.rol === RolesEnum.ADMIN;
+
+            //Renderiza la página por login true
+            return res.render('pagina', {
+                contenido: 'paginas/home',
+                session: req.session,
+            });
+
+        } catch (e) {
+            //Error de usuario o contraseña, renderiza
+            return res.render('pagina', {
+                contenido: 'paginas/login',
+                error: 'El usuario o contraseña no son válidos',
+            });
+        }
+    });
 }
 
 export function aniadirUsuario(req, res) {
